8559965d1b14a644d67340dd38503e14456d55b3
DubyD <140568071+DubyD@users.noreply.github.com>
2024-03-14 12:11:38 -0400

    Installing an engine
    Made some motor methods to handle the speed of the Car

--- Motor.java
+++ Motor.java
@@ -1,2 +1,40 @@
+//Author WD
+
+import java.util.Random;
+
 public class Motor {
+    private int speed;
+    private Tires wheels;
+
+    public Motor(){
+        this.speed = 0;
+        this.wheels = new wheels();
+    }
+
+//-------------Setters-----------------------------------------------------
+
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+//------------------Getters-------------------------------------------------
+
+    public int getSpeed(){
+
+        Random randomizer = new Random();
+
+        if(this.speed == 0){
+            this.speed = 1;
+        }else if(this.speed == 1){
+
+
+        }else if(this.speed == 2){
+
+
+        }
+
+        return this.speed;
+    }
+
+
 }
