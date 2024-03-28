39d44d4590908e41c4b2fadb2c5cfbba1da5128c
PieTw3lve <ericlim000@outlook.com>
2024-03-28 12:04:10 -0400

    Implemented GameGrid.java

--- GameGrid.java
+++ GameGrid.java
@@ -1,8 +1,58 @@
+// Author EL
 
+import java.util.List;
+
+import javax.swing.JLabel;
 
 public class GameGrid {
 
-    void updateLabels() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private JLabel[][] labels;
+
+    private int size;
+    private City gotham;
+
+    /** 
+     * Constructor for GameGrid
+     * @param size The size of the grid
+     * @param numRacers The number of racers in the game
+     */
+    public GameGrid(int size , int numRacers) {
+        this.size = size;
+        this.labels = new JLabel[size][size];
+        this.gotham = new City(size, numRacers);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                JLabel label = new JLabel();
+
+                // Code for Label properties
+                // ...
+
+                this.labels[i][j] = label;
+            }
+        }
+    }
+
+
+    /** 
+     * Updates the labels on the grid to reflect the current state of the game. 
+     */
+    public void updateLabels() {
+        List<GamePiece> board = this.gotham.getBoard();
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                JLabel label = this.labels[i][j];
+                label.setText("");
+                for (GamePiece piece : board) {
+                    if (piece.getX() == i && piece.getY() == j) {
+                        label.setText(piece.toString());
+                    }
+                }
+            }
+        }
+    }
+
+    public JLabel[][] getLabels() {
+        return this.labels;
     }
 }
